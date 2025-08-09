7f49eb34db068cd03975905e15edc3198606b3ba
muhammadaltaf123 <altafsaleem912@gmail.com>
2025-08-09 14:59:45 +0500

    Configure gh-pages for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react1project/', // اپنے repo کا نام یہاں لکھیں
+  base: '/react1project/', 
 })
