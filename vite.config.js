faee52386699ca5e459a5f34d6054e00ab7b2158
muhammadaltaf123 <altafsaleem912@gmail.com>
2025-08-09 14:00:36 +0500

    github pages config added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react1project/', // اپنے repo کا نام یہاں لکھیں
 })
